770ba1b120148f7e1c6948b4442c7c4cf8836b93
Arjun patidar <arjunpatidar9754@gmail.com>
2022-06-10 14:57:08 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SearchWeather from "./component/SearchWeather.jsx";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+    <SearchWeather />
+    </div>
+  );
+}
+
+export default App;
